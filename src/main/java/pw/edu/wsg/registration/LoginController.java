492b0319424188fff2e4d29f0bc2eba6d8b46595
Hubert Nakielski <hubert.nakielski@gmail.com>
2021-04-30 15:21:09 +0200

    Added custom login screen and shadows

---
+++ src/main/java/pw/edu/wsg/registration/LoginController.java
@@ -0,0 +1,12 @@
+package pw.edu.wsg.registration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
